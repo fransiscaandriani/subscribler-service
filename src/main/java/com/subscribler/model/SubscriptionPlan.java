1a3b6788de0cb99d689da44955f7c91cbed788f3
Fransisca Andriani <fransisca.andriani@fransisca-mac.local>
2019-08-27 17:49:02 +0800

    Rename collection name from payment_plans to subscription_plans

--- src/main/java/com/subscribler/model/SubscriptionPlan.java
+++ src/main/java/com/subscribler/model/SubscriptionPlan.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter @Setter
 
-@Document(collection = "payment_plans")
+@Document(collection = "subscription_plans")
 public class SubscriptionPlan {
     @Id
     private @NonNull String id;
