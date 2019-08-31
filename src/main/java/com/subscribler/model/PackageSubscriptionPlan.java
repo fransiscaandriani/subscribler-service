b45b69aa59c0cd83e80ca8613f622f465c8574f3
Fransisca Andriani <sisca0501@gmail.com>
2019-08-31 13:06:57 +0800

    Add Subscriber and PackageSubscriptionPlan classes

--- src/main/java/com/subscribler/model/PackageSubscriptionPlan.java
+++ src/main/java/com/subscribler/model/PackageSubscriptionPlan.java
@@ -1,4 +1,14 @@
 package com.subscribler.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class PackageSubscriptionPlan {
+    private @NonNull String packageId;
+    private @NonNull String subscriptionPlanId;
 }
--- src/main/java/com/subscribler/model/Subscriber.java
+++ src/main/java/com/subscribler/model/Subscriber.java
@@ -1,4 +1,26 @@
 package com.subscribler.model;
 
+import com.mongodb.lang.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class Subscriber {
+    @Id
+    private @NonNull String id;
+    private @NonNull String firstName;
+    private @NonNull String lastName;
+    private @NonNull String email;
+    private @NonNull String password;
+    private @Nullable String address;
+    private @Nullable String phoneNumber;
+    private @Nullable List<PackageSubscriptionPlan> packageSubscriptionPlanList;
 }
