6a4bc7ee436b0cbed5b6a05241f725cf2b8e1dae
Eko Edita Limanta <ekoeditaa@gmail.com>
2019-08-31 15:23:42 +0800

    Implement qr codes for merchant
    Signed-off-by: Eko Edita Limanta <ekoeditaa@gmail.com>

--- src/main/java/com/subscribler/controller/MerchantController.java
+++ src/main/java/com/subscribler/controller/MerchantController.java
@@ -1,10 +1,14 @@
 package com.subscribler.controller;
 
+import com.google.zxing.WriterException;
 import com.subscribler.model.Merchant;
 import com.subscribler.repository.MerchantRepository;
+import com.subscribler.service.QRGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -58,22 +62,30 @@ public class MerchantController {
         return optionalMerchant;
     }
 
-    @DeleteMapping(value = "/merchants/{merchantId}", produces = "application/json; charset=utf-8")
+    @DeleteMapping(value = "/merchants/{merchantId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String deleteMerchant(@PathVariable String merchantId) {
         boolean result = merchantRepository.existsById(merchantId);
         merchantRepository.deleteById(merchantId);
         return "{ \"success\" : " + (result ? "true" : "false") + " }";
     }
 
-    @GetMapping(value = "/merchants/{merchantId}/embed", produces = "text/plain; charset=utf-8")
-    public String getEmbedCode(@PathVariable String merchantId) {
+    @GetMapping(value = "/merchants/{merchantId}/embed/{url}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getEmbedCode(@PathVariable String merchantId, @PathVariable String url) {
         Optional<Merchant> optionalMerchant = merchantRepository.findById(merchantId);
         if (!optionalMerchant.isPresent()) {
             return null;
         }
-        // get url from merchantId
-        String url = String.format("/merchants/%s/embed", merchantId);
         String embedString = String.format("<a href=\"%s\"><div style=\"border: 5px solid #9000FF;\n  display: inline-block;\n  padding: 5px 10px;\n  border-radius: 10px;\n  margin: 5px 10px;\n  background-color: #9000FF;\n  color: white;\n  cursor: pointer;\">\n  Subscribe Now\n</div>\n</a>", url);
         return embedString;
+    }
+
+    @GetMapping(value = "/merchants/{merchantId}/qrcode/{url}", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getQrCode(@PathVariable String merchantId, @PathVariable String url) throws IOException, WriterException {
+        Optional<Merchant> optionalMerchant = merchantRepository.findById(merchantId);
+        if (!optionalMerchant.isPresent()) {
+            return null;
+        }
+        byte[] qrData = QRGeneratorService.getQRCodeImage(url);
+        return qrData;
     }
 }
--- src/main/java/com/subscribler/controller/SubscriberController.java
+++ src/main/java/com/subscribler/controller/SubscriberController.java
@@ -3,6 +3,7 @@ package com.subscribler.controller;
 import com.subscribler.model.Subscriber;
 import com.subscribler.repository.SubscriberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,16 +15,17 @@ public class SubscriberController {
     SubscriberRepository subscriberRepository;
 
     @GetMapping("/subscribers")
-    public List<Subscriber> getSubscribers(){ return subscriberRepository.findAll();}
+    public List<Subscriber> getSubscribers() {
+        return subscriberRepository.findAll();
+    }
 
     @GetMapping("/subscribers/{subscriberId}")
-    public Optional<Subscriber> getSubscriber(@PathVariable String subscriberId){
+    public Optional<Subscriber> getSubscriber(@PathVariable String subscriberId) {
         return subscriberRepository.findById(subscriberId);
     }
 
     @PostMapping("/subscribers")
-    public Subscriber addSubscriber(@RequestBody Subscriber newSubscriber)
-    {
+    public Subscriber addSubscriber(@RequestBody Subscriber newSubscriber) {
         Subscriber subscriber = new Subscriber(
                 "Placeholder ID",
                 newSubscriber.getFirstName(),
@@ -35,8 +37,7 @@ public class SubscriberController {
     }
 
     @PutMapping("/subscribers/{subscriberId}")
-    public Optional<Subscriber> updateSubscriber(@RequestBody Subscriber newSubscriber, @PathVariable String subscriberId)
-    {
+    public Optional<Subscriber> updateSubscriber(@RequestBody Subscriber newSubscriber, @PathVariable String subscriberId) {
         Optional<Subscriber> optionalSubscriber = subscriberRepository.findById(subscriberId);
         if (optionalSubscriber.isPresent()) {
             Subscriber subscriber = optionalSubscriber.get();
@@ -52,11 +53,11 @@ public class SubscriberController {
         return optionalSubscriber;
     }
 
-    @DeleteMapping(value = "/subscribers/{subscriberId}", produces = "application/json; charset=utf-8")
+    @DeleteMapping(value = "/subscribers/{subscriberId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String deleteSubscriber(@PathVariable String subscriberId) {
         boolean result = subscriberRepository.existsById(subscriberId);
         subscriberRepository.deleteById(subscriberId);
-        return "{ \"success\" : "+ (result ? "true" : "false") +" }";
+        return "{ \"success\" : " + (result ? "true" : "false") + " }";
     }
 
 }
