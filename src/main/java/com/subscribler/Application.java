890a11ec58b687a328b898650b16a6b9b60d1320
Fransisca Andriani <sisca0501@gmail.com>
2019-08-31 17:59:42 +0800

    Remove global cors setting in application
    Signed-off-by: Fransisca Andriani <sisca0501@gmail.com>

--- src/main/java/com/subscribler/Application.java
+++ src/main/java/com/subscribler/Application.java
@@ -2,23 +2,10 @@ package com.subscribler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/merchants").allowedOrigins("http://localhost:4200");
-			}
-		};
-	}
-
-
-public static void main(String[] args) {
-	SpringApplication.run(Application.class, args);
-}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
